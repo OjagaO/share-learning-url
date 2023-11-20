64aca52b22aba04a8147f0fb4310302cf3160a7a
麻生大翔 <hiroto.asou@M443-sakai.local>
2023-11-20 14:57:00 +0900

    add darkMode

--- src/components/parts/Navbar.js
+++ src/components/parts/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,14 +9,47 @@ const openDoor = <FontAwesomeIcon icon={faSignIn} />;
 const closeDoor = <FontAwesomeIcon icon={faSignOut} />;
 
 const Navbar = ({ isAuth }) => {
+    const checkboxRef = useRef(null);
+
+    const handleChange = () => {
+        if (checkboxRef.current.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+        }
+    };
+
     return (
-        <div className="navbar">
-            <nav>
-                <Link to="/" className="nav_link"><img src="./images/KakuKiji.png" alt="" /></Link>
-                <Link to="/CreatePost" className="nav_link">{pen}<span>参考URL投稿</span></Link>
-                {isAuth ? <Link to="/Logout" className="nav_link">{openDoor}<span>ログアウト</span></Link> : <Link to="/Login" className="nav_link">{closeDoor}<span>ログイン</span></Link>}
-            </nav>
-        </div>
+        <>
+            <div className="navbar">
+                <nav>
+                    <Link to="/" className="nav_link">
+                        <img src="./images/KakuKiji.png" alt="" />
+                    </Link>
+                    <Link to="/CreatePost" className="nav_link">
+                        {pen}
+                        <span>参考URL投稿</span>
+                    </Link>
+                    {isAuth ? (
+                        <Link to="/Logout" className="nav_link">
+                            {openDoor}
+                            <span>ログアウト</span>
+                        </Link>
+                    ) : (
+                        <Link to="/Login" className="nav_link">
+                            {closeDoor}
+                            <span>ログイン</span>
+                        </Link>
+                    )}
+                    <div className="dark_mode_radio">
+                        <input type="checkbox" id="modeRatio" ref={checkboxRef} onChange={handleChange} />
+                        <label htmlFor="modeRatio" className="theme-switch__label">
+                            <span></span>
+                        </label>
+                    </div>
+                </nav>
+            </div>
+        </>
     );
 };
 
